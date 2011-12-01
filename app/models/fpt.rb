db9af82c152e3821935de0bfc21d91c31fa37fd3
yechanglun <yechanglun@gmail.com>
2011-12-01 16:52:38 +0800

    创建FPT传输服务

---
+++ app/models/fpt.rb
@@ -0,0 +1,77 @@
+#encoding:UTF-8
+=begin
+  * Description:指纹信息文件传输标准相关类
+  * Author:YeChangLun
+  * Date: 2011-10-29
+=end
+
+
+
+#十指模型
+class NyzwIc < ActiveRecord::Base
+  set_table_name "nyzw_ic"
+end
+
+class NyzwFp < ActiveRecord::Base
+  set_table_name "nyzw_fp"
+end
+
+class NyzwTx < ActiveRecord::Base
+  set_table_name "nyzw_tx"
+end
+
+class NyzwTzxx < ActiveRecord::Base
+  set_table_name "nyzw_tzxx"
+end
+
+class NyzwZdyxx < ActiveRecord::Base
+  set_table_name "nyzw_zdyxx"
+end
+
+#现场模型
+class XczwIc < ActiveRecord::Base
+  set_table_name "xczw_ic"
+end
+
+class XczwFp < ActiveRecord::Base
+  set_table_name "xczw_fp"
+end
+
+class XczwTx < ActiveRecord::Base
+  set_table_name "xczw_tx"
+end
+
+class XczwTzxx < ActiveRecord::Base
+  set_table_name "xczw_tzxx"
+end
+
+class XczwZdyxx < ActiveRecord::Base
+  set_table_name "xczw_zdyxx"
+end
+
+#任务模型
+class BkTask < ActiveRecord::Base
+  set_table_name "bk_task"
+end
+
+class CcTask < ActiveRecord::Base
+  set_table_name "cc_task"
+end
+
+class DcTask < ActiveRecord::Base
+  set_table_name "dc_task"
+end
+
+class ZcTask < ActiveRecord::Base
+  set_table_name "zc_task"
+end
+
+class XcTask < ActiveRecord::Base
+  set_table_name "xc_task"
+end
+
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fts::Application.config.secret_token = 'adee0ae5b2d8043bc1f473cd90e760b2f04be2895cd3dcd9ffb68b8d68d40a2a1d94804a34ecaf38b9906e91583bc83b815027e65d9bc36fc5f51ffe6795cc18'
